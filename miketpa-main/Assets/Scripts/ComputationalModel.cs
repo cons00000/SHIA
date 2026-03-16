f379f57c936e0b94edaaef058f119fc16c8b4b5f
Constance Piquet <copiquet@gmail.com>
2026-03-16 08:36:20 +0100

    added template

---
+++ miketpa-main/Assets/Scripts/ComputationalModel.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Diagnostics;
+
+
+namespace Assets.Scripts
+{
+    public class ComputationalModel
+    {
+        //Vous pouvez déclarer tout un ensemble de variables utiles ici
+        private int nombreDeTours = 5;
+
+
+        //Vous pouvez déclarer un constructeur avec paramètre si vous voulez 
+        public ComputationalModel() { 
+            nombreDeTours = 5;
+        }
+        public ComputationalModel(int nbTours)
+        {
+            nombreDeTours = nbTours;
+        }
+
+        public void UserValues(string values)
+        {
+            nombreDeTours++;
+        }
+
+        public void LLMValues(string values)
+        {
+            nombreDeTours++;
+        }
+
+        //Ici je simule que l'émotion du système est égale au nombre de tours de parole.
+        //Pour l'exemple, je m'en sers pour demander au LLM de répondre de manière de plus en plus aggressive (il perd patience)
+        public int getEmotion()
+        {
+            return nombreDeTours;
+        }
+    }
+}
